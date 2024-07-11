b1f13b27f0bfd329bf3dfbfea8afb3c734637b0a
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2024-07-11 16:04:27 +1000

    add empty function and test cases

---
+++ mergeSort.js
@@ -0,0 +1,7 @@
+function mergeSort(arr) {
+  return; // sorted array
+}
+
+// test cases
+console.log(mergeSort([3, 2, 1, 13, 8, 5, 0, 1])); // [0, 1, 1, 2, 3, 5, 8, 13]
+console.log(mergeSort([105, 79, 100, 110])); // [79, 100, 105, 110]
