035cc7c9acabd55cd9216c6c6a16c4c3f1ce9465
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2024-07-11 16:18:22 +1000

    get right side

--- mergeSort.js
+++ mergeSort.js
@@ -2,10 +2,12 @@ function mergeSort(arr) {
   // split in half
   const halfIdx = Math.round(arr.length / 2);
 
-  console.log(arr.slice(0, halfIdx));
+  // console.log(arr.slice(0, halfIdx));
+  // console.log(arr.slice(halfIdx));
 
   // recursive sorts
   // const leftArr = mergeSort(arr.slice(0, halfIdx));
+  // const rightArr = mergeSort(arr.slice(halfIdx + 1));
 
   // merge sorted subarrays using temp array
 }
