aa68ba42a794c50b54a58d475a9c9f1d3f7e469e
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2024-07-11 13:12:04 +1000

    build fibonacci iteratively

--- fibonacci.js
+++ fibonacci.js
@@ -1,6 +1,26 @@
+// using iteration
 const fibs = (count) => {
-  return count;
-}
+  console.log(`the count is ${count}.`);
 
-fibs(3);
-fibs(10);
+  // guard clause
+  if (count <= 0 || !Number.isInteger(count)) return [];
+
+  // create placeholders
+  const res = [0].concat(new Array(count - 1).fill(1));
+  if (count < 3) return res.slice(0, count);
+
+  let idx = 2;
+  while (idx < count) {
+    res[idx] = res[idx - 1] + res[idx - 2];
+    idx++;
+  }
+
+  return res;
+};
+
+console.log(fibs(0));
+console.log(fibs(1));
+console.log(fibs(2));
+console.log(fibs(8));
+console.log(fibs(3));
+console.log(fibs(10));
